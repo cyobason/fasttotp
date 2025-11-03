e0d6f581ce96166f5b17c5aa1a5f984ce63bce1d
Jacoder <cyobason@qq.com>
2025-11-03 15:06:41 +0800

    remove statusCode

--- lib/utils/request.dart
+++ lib/utils/request.dart
@@ -5,11 +5,11 @@ import 'package:login/utils/alert.dart';
 import 'package:login/constants/locales.dart';
 
 Future<Map<String, dynamic>> request(
-  String url,
-  String command, {
-  Map<String, String>? headers,
-  bool isPost = false,
-}) async {
+    String url,
+    String command, {
+      Map<String, String>? headers,
+      bool isPost = false,
+    }) async {
   var uri = Uri.parse(url + command);
   try {
     http.Response response;
@@ -22,16 +22,13 @@ Future<Map<String, dynamic>> request(
           .post(uri, headers: headers)
           .timeout(Duration(milliseconds: connectTimeout));
     }
-    bool isSuccess = response.statusCode >= 200 && response.statusCode < 300;
-    if (isSuccess) {
-      dynamic data;
-      try {
-        data = json.decode(response.body);
-      } catch (e) {
-        data = response.body;
-      }
-      return {'statusCode': response.statusCode, 'data': data};
+    dynamic data;
+    try {
+      data = json.decode(response.body);
+    } catch (e) {
+      data = response.body;
     }
+    return {'statusCode': response.statusCode, 'data': data};
   } catch (e) {
     // Request fail.
   }
