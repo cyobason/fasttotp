92f7818ca03bf659c49ab28b40d097bb0828fa7f
Jacoder <cyobason@qq.com>
2025-11-07 14:18:09 +0800

    add timeZone

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,7 +13,6 @@ import 'package:login/constants/package.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:app_links/app_links.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:io' show Platform;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
